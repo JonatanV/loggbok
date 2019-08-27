9dee6cb87d10be9eaa7adcbb29c30072afa640d5
JonatanV <nofettatjarn1>
2019-08-27 10:30:50 +0200

    visa färdig

--- src/loggBok.java
+++ src/loggBok.java
@@ -1,2 +1,10 @@
 public class loggBok {
+    public static void main(String[] args) {
+        System.out.println( "1. Visa" +"\n"+
+                            "2. Lägg till post" + "\n"+
+                            "3. Uppdatera" +"\n"+
+                            "4. Spara" +"\n"+
+                            "5. LÄs in" + "\n" +
+                            "6. Avsluta" );
+    }
 }
