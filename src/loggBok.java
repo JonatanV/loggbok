c0d52ebb63e5141f3e4c44c93330071b7465ebd1
JonatanV <nofettatjarn1>
2019-08-27 11:06:19 +0200

    Kan välja alternativ

--- src/loggBok.java
+++ src/loggBok.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class loggBok {
     public static void main(String[] args) {
-        System.out.println( "1. Visa" +"\n"+
-                            "2. Lägg till post" + "\n"+
-                            "3. Uppdatera" +"\n"+
-                            "4. Spara" +"\n"+
-                            "5. LÄs in" + "\n" +
-                            "6. Avsluta" );
+        Scanner tgb = new Scanner(System.in);
+        System.out.println("Välj ett alternativ" + "\n"+
+                "1. Visa" +"\n"+
+                "2. Lägg till post" + "\n"+
+                "3. Uppdatera" +"\n"+
+                "4. Spara" +"\n"+
+                "5. LÄs in" + "\n" +
+                "6. Avsluta" );
+        int input = tgb.nextInt();
+
+
+        if (input == 1){
+            System.out.println("Under konstruktion");
+        }
+        if (input == 2){
+            System.out.println("Under konstruktion");
+        }
+        if (input == 3){
+            System.out.println("Under konstruktion");
+        }
+        if (input == 4){
+            System.out.println("Under konstruktion");
+        }
+        if (input == 5){
+            System.out.println("Under konstruktion");
+        }
+        if (input == 6){
+            System.out.println("Under konstruktion");
+        }
     }
 }
