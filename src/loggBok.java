72fae9028298ccd50c4101bdbf2decad9b52b757
JonatanV <nofettatjarn1>
2019-09-03 09:22:46 +0200

    Ändrade från en massa if satser till ett switch case och skapade en metod som skriver ut menyn.

--- src/loggBok.java
+++ src/loggBok.java
@@ -1,10 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class loggBok {
     public static void main(String[] args) {
-        System.out.println( "1. Visa" +"\n"+
-                            "2. Lägg till post" + "\n"+
-                            "3. Uppdatera" +"\n"+
-                            "4. Spara" +"\n"+
-                            "5. LÄs in" + "\n" +
-                            "6. Avsluta" );
+        Scanner tgb = new Scanner(System.in);
+        printMenu();
+        int input = tgb.nextInt();
+        ArrayList<String> posts = new ArrayList<String>();
+        switch (input) {
+            case 1:
+                System.out.println("Under konstruktion");
+                break;
+            case 2:
+                System.out.println("Under konstruktion");
+                break;
+            case 3:
+                System.out.println("Under konstruktion");
+                break;
+            case 4:
+                System.out.println("Under konstruktion");
+                break;
+            case 5:
+                System.out.println("Under konstruktion");
+                break;
+            case 6:
+                System.out.println("Under konstruktion");
+                break;
+            default:
+                System.out.println("Ogiltigt input");
+                break;
+        }
+    }
+
+
+    private static void printMenu(){
+        System.out.println("Välj ett alternativ" + "\n"+
+                "1. Visa" +"\n"+
+                "2. Lägg till post" + "\n"+
+                "3. Uppdatera" +"\n"+
+                "4. Spara" +"\n"+
+                "5. LÄs in" + "\n" +
+                "6. Avsluta" );
     }
 }
