902d0467f5f66c34ce70ac43b37e1c4189fd1dbd
JonatanV <nofettatjarn1>
2019-09-03 10:27:28 +0200

    KAn skriva poster i loggboken men de sparas inte gjorde allt till metoder

--- src/loggBok.java
+++ src/loggBok.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 
 public class loggBok {
     public static void main(String[] args) {
+    while(true) {
         Scanner tgb = new Scanner(System.in);
         printMenu();
         int input = tgb.nextInt();
+        String fix = tgb.nextLine();
         ArrayList<String> posts = new ArrayList<String>();
 
+        choose(tgb, input, posts);
+
+        }
+    }
+
+    private static void choose(Scanner tgb, int input, ArrayList<String> posts) {
         switch (input) {
             case 1:
                 System.out.println("Under konstruktion");
                 break;
             case 2:
-                System.out.println("Under konstruktion");
                 add(posts, tgb);
                 break;
             case 3:
@@ -27,17 +34,17 @@ public class loggBok {
                 break;
             case 6:
                 System.out.println("Under konstruktion");
+                quit();
                 break;
             default:
                 System.out.println("Ogiltigt input");
                 break;
         }
-
-
     }
 
-
-
+    private static void quit() {
+        System.exit(0);
+    }
 
 
     private static void add(ArrayList<String> posts, Scanner tgb) {
